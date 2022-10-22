297f3e46e7d9a159dbe1b7bd199cd6a6947a4e94
franioli <francesco.ioli@polimi.it>
2022-10-22 17:28:07 +0200

    fixed typo in paths for importing bundler output

--- src/sandbox.py
+++ src/sandbox.py
@@ -51,7 +51,7 @@ def add_markers(
 
 im_ext = 'jpg'
 im_path = Path('/home/francesco/lib/metashape/data/images')
-bundler_out_path = Path('/home/francesco/lib/metashape/belpy.out')
+bundler_out_path = Path('/home/francesco/lib/metashape/data/belpy.out')
 
 X = (49.6488, 192.0875, 71.7466)
 cams = ['IMG_2814', 'IMG_1289']
